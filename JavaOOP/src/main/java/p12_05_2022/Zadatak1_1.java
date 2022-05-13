2331d77006241e5cac0597a32f34ee1b9f61344c
Marija Prokic <marijaprokic@yahoo.com>
2022-05-13 12:48:13 +0200

    Zadatak1_1

--- JavaOOP/src/main/java/p12_05_2022/Zadatak1_1.java
+++ JavaOOP/src/main/java/p12_05_2022/Zadatak1_1.java
@@ -1,2 +1,7 @@
-package p12_05_2022;public class Zadatak1_1 {
+package p12_05_2022;
+
+public class Zadatak1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
